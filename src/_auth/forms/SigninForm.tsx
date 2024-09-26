5dfccd99441c950300fc41360d878d2e6b9c699f
suavel <75282935+suavel@users.noreply.github.com>
2024-09-26 16:26:55 -0400

    Add and intialize shadcn/ui library

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Button } from "@/components/ui/button"
 
 const SigninForm = () => {
   return (
-    <div>SigninForm</div>
+    <div>
+      SigninForm
+    <Button>Click me</Button>
+    </div>
+    
   )
 }
 
