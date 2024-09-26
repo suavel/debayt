339008adba1432d178372d64fa690edd4583c702
suavel <75282935+suavel@users.noreply.github.com>
2024-09-26 14:11:24 -0400

    Add export default as Home to index.ts and import pages into App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
-import { Router, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './globals.css';
+import { Home } from './_root/pages';
+import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
 
 const App = () => {
   return (
     <main className="flex h-screen">
       <Routes>
         {/* public routes */}
-        <Router path="/sign-in" element={<SigninForm />} />
+        <Route element={<AuthLayout />}>
 
+        </Route>
+        <Route path="/sign-in" element={<SigninForm />} />
+        <Route path="/sign-up" element={<SignupForm />} />
         {/* private routes */}
+        <Route element={<RootLayout />}>
+
+        </Route>
         <Route index element={<Home />}/>
       </Routes>
     </main>
